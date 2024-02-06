8cd0c1d47f7542604f7501734c907098bd00417a
matheusDev <matheus.info29@gmail.com>
2024-02-06 10:53:35 -0300

    Update

---
+++ Aula03- Laço For/ex02- imprimindo numeros paresde 1 a 100.js
@@ -0,0 +1,6 @@
+//Escreva um programa que calcule e imprima a soma dos números pares de 1 a 100
+let num =0;
+for(let cont=0; cont<=100;cont+=2){
+    num+=cont
+    console.log(`N°${num}`);
+}
--- Aula03- Laço For/node_372a1cbfb1364.tmp.js
+++
@@ -1,4 +0,0 @@
-
-for(let cont=0;cont<10;cont+2){
-    console.log(`O valor do contador é: ${cont}`);
-}
