a274aef1bb4960b07c4c2186e4754c24d6c46d77
matheusDev <matheus.info29@gmail.com>
2024-02-15 11:58:10 -0300

    Update

---
+++ Aula 05 - Arrays/ex01 - Conhecendo Arrays.js
@@ -0,0 +1,11 @@
+let frutas = ["Kiwi", "Manga", "Ameixa Vermelha"]
+
+frutas.push("Tomate")
+console.log(frutas);
+
+frutas.shift()
+frutas.shift()
+frutas.shift()
+console.log(frutas);
+frutas.push("mexerica")
+console.log(frutas);
---
+++ Aula 05 - Arrays/exe02- Percorrendo Arrays.js
@@ -0,0 +1,8 @@
+let frutas = ["Maça","Banana","Kiwi","Manga"]
+
+
+for(let cont=0;cont<frutas.length;cont++){
+    
+    console.log(frutas[cont]);
+
+}
